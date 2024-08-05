39623417b30795b76269ec60bd6746f80d3f54b9
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-08-05 10:58:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ let btnclose = document.getElementById("btnclose");
 
 btnclose.addEventListener("click", () => {
 	let category = document.getElementById("category").value;
-	let passengers = document.getElementById("passengers").value;
+	let engine_capacity = document.getElementById("engine_capacity").value;
 	let count = document.getElementById("count").value;
-	let type_ts = "car"
+	let type_ts = "bike"
 
 	let data = {
 		type_ts: type_ts,
 		category: category,
-		passengers: passengers,
+		engine_capacity: engine_capacity,
 		count: count,
 	}
 	tg.sendData(JSON.stringify(data));
