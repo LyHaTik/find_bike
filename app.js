2578b958e3a08e0c9e25a8071d47198991234a3f
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-08-05 10:46:22 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let tg = window.Telegram.WebApp;
+
+
+
+let btnclose = document.getElementById("btnclose");
+
+btnclose.addEventListener("click", () => {
+	let category = document.getElementById("category").value;
+	let passengers = document.getElementById("passengers").value;
+	let count = document.getElementById("count").value;
+	let type_ts = "car"
+
+	let data = {
+		type_ts: type_ts,
+		category: category,
+		passengers: passengers,
+		count: count,
+	}
+	tg.sendData(JSON.stringify(data));
+	tg.close;
+});
